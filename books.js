7563cc3973cd51b20c918e6ed957191abb30f6f1
Nolewp <jochs4fun@gmail.com>
2022-07-18 21:45:56 -0400

    added a local storage check function

--- books.js
+++ books.js
@@ -7,6 +7,38 @@ let getTitle = document.getElementById("title");
 let getAuthor = document.getElementById("author");
 let getRead = document.getElementById("readOrNot");
 let showForm = document.getElementById("openForm")
+// local storage
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+if (storageAvailable('localStorage')) {
+    console.log("hiza local storage")
+}
+else {
+    console.log(storageAvailable(localStorage))
+}
 
 showForm.addEventListener("click", (e) => {
     openForm()
